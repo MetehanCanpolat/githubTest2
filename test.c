c4ee5d537d04277be5327bc54763d8e59f6ee25d
MetehanCanpolat <metehancanpolat@outlook.com>
2023-12-21 13:41:13 +0300

    test

---
+++ test.c
@@ -0,0 +1,8 @@
+//
+//  test.c
+//  githubTest
+//
+//  Created by Metehan Canpolat on 21.12.2023.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  githubTest
+//
+//  Created by Metehan Canpolat on 21.12.2023.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_h */
